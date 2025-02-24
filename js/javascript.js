ba8473298720464a524965fb8f60b09e9baca69e
marcfleddermann <marcfleddermann@gmail.com>
2025-02-24 10:07:45 +0100

    Fixed mobile navigation

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,13 @@
 function showNav(){
+    
     var navList = document.getElementById("nav-list");
 
     if(navList.classList.contains("hidden")){
-
+        document.getElementById('body').style.overflow ="hidden";
         navList.classList.remove("hidden");
         navList.classList.add("flex");
     } else {
+        document.getElementById('body').style.overflow ="auto";
         navList.classList.remove("flex");
         navList.classList.add("hidden");
     }
