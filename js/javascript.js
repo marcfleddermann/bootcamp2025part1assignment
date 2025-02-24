1856cfe08d58c9bbe74e1051d44a75931b8c1e28
marcfleddermann <marcfleddermann@gmail.com>
2025-02-24 13:13:35 +0100

    Added Logo Slider and Work-Section

--- js/javascript.js
+++ js/javascript.js
@@ -4,13 +4,14 @@ function showNav(){
 
     if(navList.classList.contains("hidden")){
         document.getElementById('body').style.overflow ="hidden";
+        document.getElementById('slide-track').style.display ="none";
         navList.classList.remove("hidden");
         navList.classList.add("flex");
     } else {
         document.getElementById('body').style.overflow ="auto";
+        document.getElementById('slide-track').style.display ="flex";
         navList.classList.remove("flex");
         navList.classList.add("hidden");
     }
-    
 
 }
